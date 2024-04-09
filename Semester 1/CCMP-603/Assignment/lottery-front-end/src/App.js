6ce90bcd06f2e9168b6e9dbf9ab4321b87398077
Nam H Nguyen <namnh.developer@gmail.com>
2024-04-09 12:00:13 -0600

    Updated code for CCMP 606

--- Semester 1/CCMP-603/Assignment/lottery-front-end/src/App.js
+++ Semester 1/CCMP-603/Assignment/lottery-front-end/src/App.js
@@ -1,16 +1,20 @@
 import React from "react";
 import Web3 from "web3";
-import ContractArtifact from "./Lottery.json";
+import ContractArtifact from "./lottery.json";
 
 
 class App extends React.Component {
   state = {
     manager: "",
     players: [],
+    powerBallPlayers: [],
     balance: "",
     accountAddress: "",
     value: "",
+    powerBall: "",
+    latestPowerBallNumber: "",
     message: "",
+    contractAddress: "",
   };
 
   async componentDidMount() {
@@ -23,10 +27,18 @@ class App extends React.Component {
 
     const manager = await LotteryContract.methods.manager().call();
     const players = await LotteryContract.methods.getPlayers().call();
+    const powerBallPlayers = await LotteryContract.methods.getPowerBallPlayers().call();
+    const latestPowerBallNumber = await LotteryContract.methods.getPowerBall().call();
+    console.log(latestPowerBallNumber);
     const balance = await web3.eth.getBalance(LotteryContract.options.address);
     const accounts = await web3.eth.getAccounts();
     const accountAddress = accounts[0];
-    this.setState({ manager, players, balance, accountAddress });
+    this.setState({
+      manager,
+      players,
+      powerBallPlayers, balance, accountAddress, latestPowerBallNumber,
+      contractAddress: contractAddress
+    });
   }
 
   onSubmit = async (event) => {
@@ -40,16 +52,15 @@ class App extends React.Component {
     const accounts = await web3.eth.getAccounts();
 
     this.setState({ message: "Waiting on transaction success..." });
-
-    await LotteryContract.methods.enter().send({
+    await LotteryContract.methods.enter(this.state.powerBall).send({
       from: accounts[0],
       value: Web3.utils.toWei(this.state.value, "ether"),
     });
 
     const players = await LotteryContract.methods.getPlayers().call();
+    const powerBallPlayers = await LotteryContract.methods.getPowerBallPlayers().call();
     const balance = await web3.eth.getBalance(LotteryContract.options.address);
-    this.setState({ players, balance, message: "You have been entered!" });
-
+    this.setState({ players, powerBallPlayers, balance, message: "You have been entered!" });
   };
 
   onClick = async () => {
@@ -67,26 +78,49 @@ class App extends React.Component {
       from: accounts[0],
     });
     const players = await LotteryContract.methods.getPlayers().call();
+    const powerBallPlayers = await LotteryContract.methods.getPowerBallPlayers().call();
     const balance = await web3.eth.getBalance(LotteryContract.options.address);
-    this.setState({ players, balance, message: "A winner has been picked! Check your wallet now!" });
+    this.setState({ players, powerBallPlayers, balance, message: "A winner has been picked! Check your wallet now!" });
+  };
+
+  fetchPowerBallNumber = async () => {
+    const contractABI = ContractArtifact.abi;
+    const networkId = await window.ethereum.request({ method: "net_version" });
+    const contractAddress = ContractArtifact.networks[networkId].address;
+
+    const web3 = new Web3(window.ethereum);
+    const LotteryContract = new web3.eth.Contract(contractABI, contractAddress);
+    const accounts = await web3.eth.getAccounts();
+
+    this.setState({ message: "Waiting on transaction success..." });
+
+    await LotteryContract.methods.requestUpdate().send({
+      from: accounts[0],
+    });
+    const players = await LotteryContract.methods.getPlayers().call();
+    const powerBallPlayers = await LotteryContract.methods.getPowerBallPlayers().call();
+    const latestPowerBallNumber = await LotteryContract.methods.getPowerBall().call();
+    const balance = await web3.eth.getBalance(LotteryContract.options.address);
+    this.setState({ players, powerBallPlayers, balance, latestPowerBallNumber, message: "The latest Power Ball Number has fetched!" });
   };
 
   render() {
     return (
       <div className="container mt-5">
         <div className="container">
-          <h1 className="h1">CCMP 603 - Lottery Contract</h1>
-          <h4 className="h4">You are login as {this.state.accountAddress}</h4>
-          <h4 className="h4">
-            This contract is managed by {this.state.manager}
-          </h4>
+          <h1 className="h1">CCMP 606 - Integrated Services Using Smart Contracts</h1>
+          <h5 className="h5">You are login as <a target="_blank" rel="noreferrer" href={"https://sepolia.etherscan.io/address/" + this.state.accountAddress}>{this.state.accountAddress}</a></h5>
+          <h5 className="h5">
+            This contract is managed by <a target="_blank" rel="noreferrer" href={"https://sepolia.etherscan.io/address/" + this.state.manager}>{this.state.manager} </a>
+          </h5>
+          <h5 className="h5">This contract address is <a target="_blank" rel="noreferrer" href={"https://sepolia.etherscan.io/address/" + this.state.contractAddress}>{this.state.contractAddress}</a></h5>
           <hr></hr>
         </div>
         <div className="container">
           <form onSubmit={this.onSubmit}>
             <h4>Want to try your luck?</h4>
             <div >
-              <label>Amount of ether to enter, must be equal or more than 1 ETH</label>
+              <label>Amount of ether to enter, must be equal or more than 0.1 ETH</label>
               <div className="input-group flex-nowrap">
                 <span className="input-group-text" id="addon-wrapping">ETH</span>
                 <input
@@ -95,6 +129,19 @@ class App extends React.Component {
                   className="form-control"
                   value={this.state.value}
                   onChange={(event) => this.setState({ value: event.target.value })}
+                />
+              </div>
+            </div>
+            <div >
+              <label>PowerBall Number</label>
+              <div className="input-group flex-nowrap">
+                <span className="input-group-text" id="addon-wrapping">PowerBall</span>
+                <input
+                  type="text"
+                  placeholder="Your PowerBall Number"
+                  className="form-control"
+                  value={this.state.powerBall}
+                  onChange={(event) => this.setState({ powerBall: event.target.value })}
                 />
               </div>
             </div>
@@ -111,6 +158,16 @@ class App extends React.Component {
             </div>
             : ""}
 
+          {this.state.accountAddress === this.state.manager ?
+            <div>
+              <h4>Fetching latest Powerl Ball Number</h4>
+              <button type="button" className="btn btn-danger text-white btn-lg" onClick={this.fetchPowerBallNumber}>Request Oracle The Latest PowerBall Number!</button>
+              <hr />
+            </div>
+            : ""}
+
+          {this.state.latestPowerBallNumber && this.state.manager === this.state.accountAddress ? <div class="">The latest Power Ball Number is <span class="h1">{Number(this.state.latestPowerBallNumber)}</span><hr></hr></div> : ""}
+
           {this.state.message ?
             <div>
               <h1>{this.state.message}</h1>
@@ -120,8 +177,8 @@ class App extends React.Component {
           <p>There are currently {this.state.players.length} people entered, competing to win {Web3.utils.fromWei(this.state.balance, "ether")} ether!</p>
           <p>List and index of players:</p>
           <ol type="number">
-            {this.state.players.map((player) => (
-              <li key={player}>{player}</li>
+            {this.state.players.map((player, index) => (
+              <li key={index}>{player} - {Number(this.state.powerBallPlayers[index])}</li>
             ))}
           </ol>
         </div>
